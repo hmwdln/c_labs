e29525529ecad4e7bfd0fd2b8d17f8bd574281a8
hmwdln <grishenkonikolay@yandex.ru>
2022-10-29 18:38:44 +0500

    добавил 4 лабу

--- lab4.c
+++ lab4.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <math.h>
 
-f(int n)
+double f(int n)
 {
-    double pi1, h, x, integral;
+    double pi1, h, x, integral = 0;
     pi1 = M_PI / 2;
-    h = pi1 / n;
+    h = M_PI / n;
     for (x = pi1 / n; x <= M_PI; x += h)
     {
         double fun;
@@ -13,15 +13,26 @@ f(int n)
             fun = pow(2, x) - 2 + pow(x, 2);
         else
             fun = sqrt(x) * exp(-pow(x, 2));
-        integral += fun
+        integral += fun;
     }
+    integral *= h;
+    return integral;
 }
 
 int main()
 {
-    /* if (0 <= x && x <= (M_PI / 2))
-        f = pow(2, x) - 2 + pow(x, 2);
-    else if ((M_PI / 2) < x && x <= M_PI)
-        f = sqrt(x) * exp(-pow(x, 2));
-        printf("x = %lf\t f(x) = %lf\n", x, f); */
+    double e;
+    printf("Введите точность => ");
+    scanf("%lf", &e);
+    int n = 1;
+    double integral_1 = 0;
+    double integral_2 = 1;
+    while ((fabs(integral_2 - integral_1) / 3) >= e)
+    {
+            n *= 2;
+            integral_1 = integral_2;
+            integral_2 = f(n * 2);
+    }
+    printf("%lf\n", integral_2);
+    return 0;
 }
